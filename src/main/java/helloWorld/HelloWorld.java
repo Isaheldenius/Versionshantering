cdfa772903a34a048e7bb6bae07c7f8b0f129689
Emelie Ilola <emelie.ilola@outlook.com>
2026-03-30 11:49:47 +0200

    Emelie ändring

--- src/main/java/helloWorld/HelloWorld.java
+++ src/main/java/helloWorld/HelloWorld.java
@@ -2,6 +2,9 @@ package helloWorld;
 
 public class HelloWorld {
     public static void main(String[] args) {
+
         System.out.println("Hello Nina");
+
+        System.out.println("Hello from Emelie");
     }
 }
