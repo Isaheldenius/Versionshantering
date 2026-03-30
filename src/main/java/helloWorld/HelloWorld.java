7913f6bab32ae34e4b812bfb94728ee2642fdc1a
Isaheldenius <isaheldenius@hotmail.com>
2026-03-30 10:57:23 +0200

    Update HelloWorld.java

--- src/main/java/helloWorld/HelloWorld.java
+++ src/main/java/helloWorld/HelloWorld.java
@@ -2,6 +2,6 @@ package helloWorld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Isa");
+        System.out.println("Hello Erik");
     }
 }
