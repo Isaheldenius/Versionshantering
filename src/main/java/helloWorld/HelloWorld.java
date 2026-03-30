e54ab1824afa34b7c180d73c54a2eca14c495975
nova_ <nova_jansson@hotmail.com>
2026-03-30 12:01:55 +0200

    Sanna

--- src/main/java/helloWorld/HelloWorld.java
+++ src/main/java/helloWorld/HelloWorld.java
@@ -2,6 +2,6 @@ package helloWorld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Nina");
+        System.out.println("Hello Sanna!");
     }
 }
